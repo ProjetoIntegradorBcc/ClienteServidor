4c6d2520228f744414a139cf6ce4247b4986031a
Lucas Firak <lucasfirak@hotmai.com>
2014-07-29 11:08:31 -0300

    Atualisacao da Script do banco
    adaptações feitas para ser compativel com mysql

--- banco/sqlBancoServidor.sql
+++ banco/sqlBancoServidor.sql
@@ -1,52 +1,47 @@
 CREATE TABLE Alunos(
-RA int(10),
-nome varchar(50),
-idade int(5),
-endereco varchar(150),
-curso varchar(40),
-anoDeEntradaCurso int(5),
-disciplinasMatriculadas varchar(100),
-disciplinasConcluidas varchar(100),
-PRIMARY KEY (RA)
+RA integer Integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nome Varchar(50),
+idade Integer,
+endereco Varchar(150),
+curso Varchar(40),
+anoDeEntradaCurso Integer,
+disciplinasMatriculadas Varchar(100),
+disciplinasConcluidas Varchar(100)
 );
 
 CREATE TABLE Professor(
-RA int(10),
-nome varchar(50),
-idade int(5),
-endereco varchar(150),
-departamento varchar(40),
-disciplinasMinistra varchar(100),
-linhasPesquisa varchar(100),
-PRIMARY KEY (RA)
+RA integer Integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nome Varchar(50),
+idade Integer,
+endereco Varchar(150),
+departamento Varchar(40),
+disciplinasMinistra Varchar(100),
+linhasPesquisa Varchar(100)
 );
 
 CREATE TABLE Disciplinas(
-idDisciplina int(10),
-titulo varchar(50),
-preRequisitos varchar(150),
-metodoAvaliacao varchar(150),
-ementa varchar(400),
-disciplinasDependentes varchar(150),
-PRIMARY KEY (idDisciplina)
+idDisciplina integer Integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
+titulo Varchar(50),
+preRequisitos Varchar(150),
+metodoAvaliacao Varchar(150),
+ementa Varchar(400),
+disciplinasDependentes Varchar(150)
 );
 
 CREATE TABLE Salas(
-idSala int(10),
-descricaoLaboratorio varchar(50),
-numeroComputadores int(50),
-recursosDidaticos varchar(150),
-departamento varchar(50),
-capacidadeMaxima int(10),
-PRIMARY KEY (idSala)
+idSala integer Integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
+descricaoLaboratorio Varchar(50),
+numeroComputadores Integer,
+recursosDidaticos Varchar(150),
+departamento Varchar(50),
+capacidadeMaxima Integer
 );
 
 CREATE TABLE Aulas(
-idAula int(10),
-conteudoProgramatico varchar(510),
-disciplinaAssociada varchar(150),
-salaAulaAssociada varchar(150),
-alunosPresentes varchar(150),
-PRIMARY KEY (idAula)
+idAula integer Integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
+conteudoProgramatico Varchar(510),
+disciplinaAssociada Varchar(150),
+salaAulaAssociada Varchar(150),
+alunosPresentes Varchar(150)
 );
 
