e4e99ab8fc2fe6b9f2a991326da5e7bd60101faa
raafah13 <raafah13@hotmail.com>
2014-08-05 11:55:16 -0300

    AlunosVO criado
    Criação da classeAlunosVO pra desenvolvimento to projeto

---
+++ src/vo/AlunosVO.java
@@ -0,0 +1,149 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vo;
+
+/**
+ *
+ * @author Rafael
+ */
+public class AlunosVO {
+    
+    private String cod;
+    private String RA;
+    private String nome;
+    private String datanasc;
+    private String endereco;
+    private String curso;
+    private String anodeEntrada;
+    private String disciplinasMatriculadas;
+    private String DisciplinasConcluidas;
+
+    /**
+     * @return the cod
+     */
+    public String getCod() {
+        return cod;
+    }
+
+    /**
+     * @param cod the cod to set
+     */
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    /**
+     * @return the RA
+     */
+    public String getRA() {
+        return RA;
+    }
+
+    /**
+     * @param RA the RA to set
+     */
+    public void setRA(String RA) {
+        this.RA = RA;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the datanasc
+     */
+    public String getDatanasc() {
+        return datanasc;
+    }
+
+    /**
+     * @param datanasc the datanasc to set
+     */
+    public void setDatanasc(String datanasc) {
+        this.datanasc = datanasc;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+
+    /**
+     * @param curso the curso to set
+     */
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    /**
+     * @return the anodeEntrada
+     */
+    public String getAnodeEntrada() {
+        return anodeEntrada;
+    }
+
+    /**
+     * @param anodeEntrada the anodeEntrada to set
+     */
+    public void setAnodeEntrada(String anodeEntrada) {
+        this.anodeEntrada = anodeEntrada;
+    }
+
+    /**
+     * @return the disciplinasMatriculadas
+     */
+    public String getDisciplinasMatriculadas() {
+        return disciplinasMatriculadas;
+    }
+
+    /**
+     * @param disciplinasMatriculadas the disciplinasMatriculadas to set
+     */
+    public void setDisciplinasMatriculadas(String disciplinasMatriculadas) {
+        this.disciplinasMatriculadas = disciplinasMatriculadas;
+    }
+
+    /**
+     * @return the DisciplinasConcluidas
+     */
+    public String getDisciplinasConcluidas() {
+        return DisciplinasConcluidas;
+    }
+
+    /**
+     * @param DisciplinasConcluidas the DisciplinasConcluidas to set
+     */
+    public void setDisciplinasConcluidas(String DisciplinasConcluidas) {
+        this.DisciplinasConcluidas = DisciplinasConcluidas;
+    }
+    
+}
