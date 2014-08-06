c402120bd5ef98b469f8894eee30c883ddf8d072
LumaLopes <luminha_alves@hotmail.com>
2014-08-06 11:35:08 -0300

    Criação dos pacotes de Teste
    criação da classe de teste ClienteProfessor

---
+++ test/src/Cliente/ClienteProfessor.java
@@ -0,0 +1,27 @@
+package Cliente;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+/**
+ *
+ * @author Luma
+ */
+@RunWith(JUnit4.class)
+public class ClienteProfessor {
+    /**
+     * Classe de Teste para o Cliente Professor.
+     */
+    @Test
+    /**
+     * Testando
+     */
+    
+    public void testConstrutorComParametros ( ) {
+        org.junit.Assert.assertEquals(
+            "testando para ver se esta legal",
+            5, 5);
+    }
+}
