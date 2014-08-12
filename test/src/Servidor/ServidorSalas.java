fc21a94009e8eabbbce1009942d390171235654c
Jonas Vieira de Assis <jonasva7@gmail.com>
2014-08-12 03:33:02 -0300

    Alguns testes da classe servidor

--- test/src/Servidor/ServidorSalas.java
+++ test/src/Servidor/ServidorSalas.java
@@ -1,17 +1,19 @@
 package Servidor;
 
-import Cliente.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
 /**
  *
  * @author Luma
  */
 @RunWith(JUnit4.class)
 public class ServidorSalas {
+
     /**
      * Classe de Teste para o Servidor Professor.
      */
@@ -19,10 +21,25 @@ public class ServidorSalas {
     /**
      * Testando
      */
-    
-    public void testConstrutorComParametros ( ) {
-        org.junit.Assert.assertEquals(
-            "testando para ver se esta legal",
-            5, 5);
+    public final void inserindoBanco() {
+        SGDB.Salas salas = new SGDB.Salas();
+        assertEquals("Teste de retorno insercao certa", 0,
+                salas.inserir("Lab2221", "20",
+                        "Nenhum recurso", "DAFIS", "42"));
+    }
+
+    @Test
+    public final void editandoBanco() {
+        SGDB.Salas salas = new SGDB.Salas();
+        assertEquals("Teste de retorno edicao certa", 0,
+                salas.editar("1", "Lab2221", "20",
+                        "Nenhum recurso", "DAFIS", "42"));
+    }
+
+    @Test
+    public final void detetandoBanco() {
+        SGDB.Salas salas = new SGDB.Salas();
+        assertEquals("Teste de retorno delecao certa", 0,
+                salas.deletar("1"));
     }
 }
